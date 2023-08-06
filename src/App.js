e06b42c7c86465fd80adbd322df4671116b43879
Peyush3 <97232897+Peyush3@users.noreply.github.com>
2023-08-06 12:30:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter , Routes ,Route } from "react-router-dom";
+import { BrowserRouter ,Navigate ,Routes ,Route } from "react-router-dom";
 import HomePage from "scenes/homePage/index.jsx";
 import LoginPage from "scenes/loginPage/index.jsx";
 import ProfilePage from "scenes/profilePage/index.jsx";
@@ -15,7 +15,7 @@ function App() {
 
   const mode = useSelector((state)=> state.mode);
   const theme = useMemo(()=>createTheme(themeSettings(mode)) , [mode]);
-  
+  const isAuth= Boolean(useSelector((state) => state.token ));
   return (
     <div className="app">
      <BrowserRouter>
@@ -23,8 +23,8 @@ function App() {
       <CssBaseline />
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route path="/home" element={isAuth ?<HomePage /> : <Navigate to="/" />} />
+        <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
         <Route path="/navbar" element={<Navbar />} />
       </Routes>
      </ThemeProvider>
